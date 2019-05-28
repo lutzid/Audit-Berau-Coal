ff9529c734666640440879d9c167b563cecfdf99
lutzid <lut.zidan@gmail.com>
2019-05-28 12:44:24 +0700

    update homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,126 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        @include('layouts.header')
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <div id="page-container" class="sidebar-inverse side-scroll page-header-fixed page-header main-content-boxed">
+            <!-- Sidebar -->
+            <!-- END Sidebar -->
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+            <!-- Header -->
+            <header id="page-header">
+                <!-- Header Content -->
+                <div class="content-header">
+                    <!-- Left Section -->
+                    
+                    <a class="font-w700 mr-5" href="index.html">
+                        <span class="font-size-xl text-primary">Berau Coal</span>
+                    </a>
+                    <!-- END Left Section -->
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                    <!-- Middle Section -->
+                    <!-- END Middle Section -->
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <!-- Right Section -->
+                    <div class="content-header-section">
+                        <!-- Color Themes + A few of the many header options (used just for demonstration) -->
+                        <!-- Themes functionality initialized in Codebase() -> uiHandleTheme() -->
+                        <!-- END Color Themes + A few of the many header options -->
+                        <div class="content-header-section">
+                            <img class="img-fluid" src="{{asset('assets/codebase/assets/img/logos/berauwhite.png')}}">
+                        </div>
+                        <!-- END Toggle Sidebar -->
+                    </div>
+                    <!-- END Right Section -->
                 </div>
-            </div>
+                <!-- END Header Content -->
+
+                <!-- Header Search -->
+                <!-- END Header Search -->
+
+                <!-- Header Loader -->
+                <!-- Please check out the Activity page under Elements category to see examples of showing/hiding it -->
+                <div id="page-header-loader" class="overlay-header bg-primary">
+                    <div class="content-header content-header-fullrow text-center">
+                        <div class="content-header-item">
+                            <i class="fa fa-sun-o fa-spin text-white"></i>
+                        </div>
+                    </div>
+                </div>
+                <!-- END Header Loader -->
+            </header>
+            <!-- END Header -->
+
+            <!-- Main Container -->
+            <main id="main-container">
+                <!-- Page Content -->
+                <div class="content content-full">
+                    <div class="row">
+                        <div class="col-sm">
+                            <div>
+                                <div class="block">
+                        <div class="block-header block-header-default">
+                            <h3 class="block-title">Login</h3>
+                        </div>
+                        <div class="block-content">
+                            <div class="row justify-content-center py-20">
+                                <div class="col-xl-6">
+                                    <!-- jQuery Validation (.js-validation-bootstrap class is initialized in js/pages/be_forms_validation.js) -->
+                                    <!-- For more examples you can check out https://github.com/jzaefferer/jquery-validation -->
+                                    <form class="js-validation-bootstrap" action="be_forms_validation.html" method="post">
+                                        <div class="form-group row">
+                                            <label class="col-lg-4 col-form-label" for="val-username">Username <span class="text-danger">*</span></label>
+                                            <div class="col-lg-8">
+                                                <input type="text" class="form-control" id="val-username" name="val-username" placeholder="Username..">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-lg-4 col-form-label" for="val-password">Password <span class="text-danger">*</span></label>
+                                            <div class="col-lg-8">
+                                                <input type="password" class="form-control" id="val-password" name="val-password" placeholder="Password..">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <div class="col-lg-8 ml-auto">
+                                                <button type="submit" class="btn btn-alt-primary">Submit</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                            </div>
+                        </div>
+                        <div class="col-sm">
+                            <div class="block block-rounded bg-image" style="background-image: url({{asset('assets/codebase/assets/img/photos/minetruck.png')}});">
+                                <div class="block-content">
+                                    <p class="text-right py-200">
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- END Page Content -->
+            </main>
+            <!-- END Main Container -->
+
+            <!-- Footer -->
+            <footer id="page-footer" class="opacity-0">
+                <!-- <div class="content py-20 font-size-xs clearfix">
+                    <div class="float-right">
+                        Crafted with <i class="fa fa-heart text-pulse"></i> by <a class="font-w600" href="http://goo.gl/vNS3I" target="_blank">pixelcave</a>
+                    </div>
+                    <div class="float-left">
+                        <a class="font-w600" href="https://goo.gl/po9Usv" target="_blank">Codebase 2.0</a> &copy; <span class="js-year-copy">2017</span>
+                    </div>
+                </div> -->
+                <a class="font-w600" href="https://goo.gl/po9Usv">Tentang kita</a>
+            </footer>
+            <!-- END Footer -->
         </div>
     </body>
 </html>
